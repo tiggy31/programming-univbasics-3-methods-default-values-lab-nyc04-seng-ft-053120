d6bee54a89e072587aab5ea62b0a5e8612e82830
TIGGY ALEMU <tigist_alemu@fitnyc.edu>
2020-06-02 21:27:52 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,7 @@
 
 def meal_choice(veg1,veg2,protein= "Tofu")
  puts "What a nutritious meal!"
- 
+ love = "A plate of #{protein} with #{veg1} and #{veg2}.
+ puts "A plate of #{protein} with #{veg1} and #{veg2}."
  return 
 end
